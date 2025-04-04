f2618d7dfad8c56f92583c9c0715e511507b63de
ViniciusAugustoSilva <viniciusaugustosantos@hotmail.com>
2025-04-04 10:12:59 -0300

    Adicionando alertsWindow

---
+++ Desafio/cypress/support/locators/locatior_alertsWindows.js
@@ -0,0 +1,6 @@
+export const locator_forms = {
+    btnBrowserWindows: '//span[contains(., "Browser Windows")]',
+    btnNewWindow: 'button[id="windowButton"]',
+    popupNewPage: 'h1[id="sampleHeading"]',
+    
+};
---
+++ Desafio/cypress/support/pageObject/alertsWindows.js
@@ -0,0 +1,37 @@
+const locators_alertsWindow = require ("../locators/locatior_alertsWindows").locators_alertsWindow;
+
+export class FormsPage {
+
+    
+
+    clickOnPracticeForm(){
+        cy.get(locators_alertsWindow.btnB).click();
+    }
+
+    
+
+    fillForm(){
+        cy.get(locators_forms.inputFirstName).type(this.userData.firstName);
+        cy.get(locators_forms.inputLastName).type(this.userData.lastName);
+        cy.get(locators_forms.inputEmail).type(this.userData.firstName + this.userData.lastName + '@email.com');
+        cy.get(locators_forms.radioBtnGender).click();
+        cy.get(locators_forms.inputMobile).type(this.userData.mobile);
+        cy.get(locators_forms.inputLastName).type('25 Jan 1990');
+        cy.get(locators_forms.inputSubject).type('test subject');
+        cy.get(locators_forms.checkbox_hobbies_music).click();
+        this.selectFile();
+        cy.get(locators_forms.address).type(this.userData.currentAddress);
+        cy.get(locators_forms.dropdown_state).select(1);
+        cy.get(locators_forms.dropdown_city).select(1);
+        cy.get(locators_forms.btnSubmit).click();
+
+    }
+
+    selectFile() {
+        const filePath = 'cypress\files\desafio.txt'; 
+    
+        cy.get("input[id='uploadPicture']").attachFile(filePath);
+        cy.wait(10000); 
+    }
+
+}
